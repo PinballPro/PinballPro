93c839d0f68dd8944c52935ab1e1a974cec7b8fe
Penistrong <770560618@qq.com>
2018-07-03 13:09:15 +0800

    ballPro类初始形态

--- PinballPro/PinballPro/ballPro.cpp
+++ PinballPro/PinballPro/ballPro.cpp
@@ -10,4 +10,27 @@ class BallPro {
 private:
 	Cordinate ball_location;
 	string ball_name;
+	int ball_status;
+public:
+	void setCordinate(Cordinate ball_location) {
+		this->ball_location = ball_location;
+	}
+	Cordinate getCordinate(void) {
+		return this->ball_location;
+	}
+	void setBallName(string ball_name) {
+		this->ball_name = ball_name;
+	}
+	string getBallName(void) {
+		return this->ball_name;
+	}
+	void setBallStatus(int ball_status) {
+		this->ball_status = ball_status;
+	}
+	int getBallStatus(void) {
+		return this->ball_status;
+	}
 };
+
+
+
